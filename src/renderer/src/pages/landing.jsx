7ac3faf1ebf4613851da064d82316649c7ec62ae
CptAstro <70530319+CptAstro@users.noreply.github.com>
2023-11-04 01:44:58 -0500

    Added play button

--- src/renderer/src/pages/landing.jsx
+++ src/renderer/src/pages/landing.jsx
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom"
 
 export function Landing() {
+
+    function handleClick() {
+        alert("Button has been clicked")
+    }
+
     return (
         <div className="flex flex-col w-screen h-screen justify-center items-center">
             <h1 className="text-4xl text-white font-bold">Landing Page</h1>
@@ -8,6 +13,9 @@ export function Landing() {
                 <Link to="/home">
                     Go to Home page
                 </Link>
+            </button>
+            <button onClick={handleClick} className="p-4 m-4 border-2 border-white rounded-lg">
+                Play
             </button>
         </div>
     )
