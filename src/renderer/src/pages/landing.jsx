a08be505509000752e26d211b1ec83886f33e9db
CptAstro <70530319+CptAstro@users.noreply.github.com>
2023-11-05 02:32:33 -0600

    Removed extraneous files and changed frontend route

--- src/renderer/src/pages/landing.jsx
+++ src/renderer/src/pages/landing.jsx
@@ -1,13 +1,7 @@
-import { Link } from "react-router-dom"
 import { LoginButton } from "../components/buttons/login-button"
 import Logo from "../../../../TigerLogo.png"
 
 export function Landing() {
-
-    function handleClick() {
-        alert("Button has been clicked")
-    }
-
     return (
         <div className="flex flex-col w-auto h-screen justify-center items-center bg-gradient-to-br from-orange1 to-orange2">
             <h1 className="titleSequence">Social Duel</h1>
