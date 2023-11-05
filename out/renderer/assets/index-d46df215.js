3239aa41b79edcb4c01d8458f9f3c8063f1bee48
CptAstro <70530319+CptAstro@users.noreply.github.com>
2023-11-05 05:11:36 -0600

    Production build

--- out/renderer/assets/index-d46df215.js
+++ out/renderer/assets/index-d46df215.js
@@ -10689,7 +10689,7 @@ client.createRoot(document.getElementById("root")).render(
     {
       domain: "dev-m2hmp2fkbivz6uc5.us.auth0.com",
       clientId: "nwDxxJjoJ7kB8gyydqhxwk82co0OX0ih",
-      authorizationParams: { redirectUri: "http://localhost:5173/#/home" },
+      authorizationParams: { redirectUri: "https://googlecloud.seannotseen.com/#/home" },
       children: /* @__PURE__ */ jsxRuntimeExports.jsx(App, {})
     }
   )
